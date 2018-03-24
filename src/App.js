9ae94de31396da0f35bdc0f9ebe1a4cd7d645ae2
Brian Azizi <briana@theodo.co.uk>
2018-03-24 15:57:31 +0000

    Setup navigation services

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import { SwitchNavigator } from 'react-navigation';
 
+import NavigatorService from './navigator';
 import WelcomeStack from './components/WelcomeStack';
 
-const App = new SwitchNavigator({
+const BaseNavigator = new SwitchNavigator({
   WelcomeStack: { screen: WelcomeStack },
 });
+
+const App = () => (
+  <BaseNavigator
+    ref={navigatorRef => {
+      NavigatorService.setContainer(navigatorRef);
+    }}
+  />
+);
 
 export default App;
