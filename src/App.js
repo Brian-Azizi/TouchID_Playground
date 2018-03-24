c4065c882ffdae0ae766fa4dd4ecab48cc2c98f2
Brian Azizi <briana@theodo.co.uk>
2018-03-24 19:25:51 +0000

    Setup redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { SwitchNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
+import reducer from './reducer';
 import NavigatorService from './navigator';
 import WelcomeStack from './components/WelcomeStack';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const BaseNavigator = new SwitchNavigator({
   WelcomeStack: { screen: WelcomeStack },
 });
 
 const App = () => (
-  <BaseNavigator
-    ref={navigatorRef => {
-      NavigatorService.setContainer(navigatorRef);
-    }}
-  />
+  <Provider store={store}>
+    <BaseNavigator
+      ref={navigatorRef => {
+        NavigatorService.setContainer(navigatorRef);
+      }}
+    />
+  </Provider>
 );
 
 export default App;
