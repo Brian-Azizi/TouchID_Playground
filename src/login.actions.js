7dc0457660505c13232bfb6abb6b06ba662117a3
Brian Azizi <briana@theodo.co.uk>
2018-03-24 19:58:59 +0000

    Navigate to HomeStack after successful login

--- src/login.actions.js
+++ src/login.actions.js
@@ -1,4 +1,5 @@
 import { login } from './api';
+import NavigatorService from './navigator';
 
 export const LOGIN = {
   REQUEST: 'LOGIN.REQUEST',
@@ -9,6 +10,9 @@ export const LOGIN = {
 export const fetchLogin = (username, password) => dispatch => {
   dispatch({ type: LOGIN.REQUEST });
   login(username, password)
-    .then(userToken => dispatch({ type: LOGIN.SUCCESS, username, userToken }))
+    .then(userToken => {
+      dispatch({ type: LOGIN.SUCCESS, username, userToken });
+      NavigatorService.navigate('HomeStack');
+    })
     .catch(error => dispatch({ type: LOGIN.ERROR, error: error.message }));
 };
